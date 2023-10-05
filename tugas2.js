41ab5795b6ea2bca05f072a16a8c6ed4093e33bb
Sandy130802 <147012946+Sandy130802@users.noreply.github.com>
2023-10-05 21:50:23 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,10 @@
+const user ={
+    firstName : "Sandy",
+    lastName : "Wijaya",
+    age : 21,
+    isMarried : true,
+
+};
+
+console.log(`Hallo, Nama saya ${user.firstName} ${user.lastName}`);
+console.log(`Umur saya ${user.age} tahun`);
